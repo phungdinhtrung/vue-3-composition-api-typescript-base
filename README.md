c51f16b625fb997b18bfdcea182986f683d7e909
Phung Dinh Trung <phungdinhtrung@gfd.com.vn>
2023-01-18 18:42:03 +0700

    Finish to add library and setup base structures

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# vue-ts-course
-Vue 3 - Typescript - Course
+# vue 3 compisition API and TypeScript
+
+## Library
+•	Store: pinia
+•	Vue 3 composition API
+•	Typescript
+•	Vue - router 4: 
+•	CSS: tailwindcss
+•	AutoImport
+•	I18n
+
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Router from '@/router/index'
+import i18n from '@/plugins/i18n'
+
+import '@/assets/style.css'
+
+
+const app = createApp(App)
+app.use(i18n);
+app.use(Router);
+app.use(PrimeVue);
+app.mount('#app')
